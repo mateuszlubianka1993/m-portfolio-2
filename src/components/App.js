1c91e1f953ef483e29224367e937096005de292a
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-09-19 18:18:57 +0200

    add BrowserRouter Switch and Routes in App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Home from './Home';
 import NavBar from './NavBar';
-
+import About from './About';
 
 const App = () => {
     return(
         <div className="app-container">
-            <NavBar />
-            <Home />
+            <BrowserRouter>
+                <div>
+                    <NavBar />
+                    <Switch>
+                       <Route path="/" exact component={Home} /> 
+                       <Route path="/about" component={About} /> 
+                    </Switch>
+                </div>
+            </BrowserRouter>
         </div>
     );
 };
