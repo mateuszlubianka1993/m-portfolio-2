c2f12ebf38b18eec01ef82098d611126e452fab1
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-09-19 12:19:19 +0200

    create Home component

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Home from './Home';
 
 const App = () => {
     return(
         <div className="app-container">
-            App
+            <Home />
         </div>
     );
 };
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="home-container">
+            <h1>Mateusz Łubianka</h1>
+            <h2>Front End Developer</h2>
+        </div>
+    )
+}
+
+export default Home
