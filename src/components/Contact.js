e1334cb779e3d43f403c5447cfb147d27fb258d9
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-09-23 11:28:04 +0200

    create Contact.css file and add some styles

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import '../style/Contact.css';
 
 const Contact = () => {
     return (
         <div className="contact-container">
             <div className="contact-header">
                 <h2>Contact me</h2>
+                <p>lubiankamateusz@gmail.com</p>
             </div>
         </div>
     )
