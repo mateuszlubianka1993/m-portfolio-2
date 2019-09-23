5c6781c9db09337e31e41867f77a1652ab2552c5
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-09-23 15:06:36 +0200

    add styles to Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,6 +4,8 @@ import '../style/Home.css';
 const Home = () => {
     return (
         <div className="home-container">
+            <div className="home-mask-left"><div className="mask-border-left"></div></div>
+            <div className="home-mask-right"><div className="mask-border-right"></div></div>
             <header className="home-header">
                 <div className="header-txt">
                     <h1>Mateusz Łubianka</h1>
