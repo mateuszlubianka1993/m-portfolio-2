26f2d6f59a28096033fe1ee513089d6c7387c931
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-09-23 11:22:23 +0200

    add contact-header in Contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,7 +3,9 @@ import React from 'react'
 const Contact = () => {
     return (
         <div className="contact-container">
-            Contact
+            <div className="contact-header">
+                <h2>Contact me</h2>
+            </div>
         </div>
     )
 }
