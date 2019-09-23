ceec5ce9a5b1c2e5198288d444c6d8a7becf570b
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-09-23 12:51:34 +0200

    create social media buttons in Contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,6 +8,44 @@ const Contact = () => {
                 <h2>Contact me</h2>
                 <p>lubiankamateusz@gmail.com</p>
             </div>
+            <div className="social-container">
+                <div className="social-box">
+                    <a href="#">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span class="fab fa-github"></span>
+                    </a>
+                </div>
+                <div className="social-box">
+                    <a href="#">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span class="fab fa-linkedin-in"></span>
+                    </a>
+                </div>
+                <div className="social-box">
+                    <a href="#">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span class="fab fa-twitter"></span>
+                    </a>
+                </div>
+                <div className="social-box">
+                    <a href="#">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span class="fab fa-facebook-f fb-span"></span>
+                    </a>
+                </div>
+            </div>
         </div>
     )
 }
