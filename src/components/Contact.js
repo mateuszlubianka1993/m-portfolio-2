9d7cd9a4245a2e3a068bb1ececb591d317ed6e7e
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-09-23 14:15:36 +0200

    update Contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,46 +4,48 @@ import '../style/Contact.css';
 const Contact = () => {
     return (
         <div className="contact-container">
-            <div className="contact-header">
-                <h2>Contact me</h2>
-                <p>lubiankamateusz@gmail.com</p>
-            </div>
-            <div className="social-container">
-                <div className="social-box">
-                    <a href="#">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span class="fab fa-github"></span>
-                    </a>
+            <div className="contact-mask">
+                <div className="contact-header">
+                    <h2>Contact me</h2>
+                    <p>lubiankamateusz@gmail.com</p>
                 </div>
-                <div className="social-box">
-                    <a href="#">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span class="fab fa-linkedin-in"></span>
-                    </a>
-                </div>
-                <div className="social-box">
-                    <a href="#">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span class="fab fa-twitter"></span>
-                    </a>
-                </div>
-                <div className="social-box">
-                    <a href="#">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span class="fab fa-facebook-f fb-span"></span>
-                    </a>
+                <div className="social-container">
+                    <div className="social-box">
+                        <a href="#">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span class="fab fa-github"></span>
+                        </a>
+                    </div>
+                    <div className="social-box">
+                        <a href="#">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span class="fab fa-linkedin-in"></span>
+                        </a>
+                    </div>
+                    <div className="social-box">
+                        <a href="#">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span class="fab fa-twitter"></span>
+                        </a>
+                    </div>
+                    <div className="social-box">
+                        <a href="#">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                            <span class="fab fa-facebook-f fb-span"></span>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
