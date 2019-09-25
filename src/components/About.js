f4022af5d51f9953c3388f4dc5cb1be02f0f7a88
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-09-25 13:33:15 +0200

    add example content, photo and styles to About component

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,46 @@
 import React from 'react'
+import '../style/About.css';
 
 const About = () => {
     return (
         <div className="about-container">
-            <div className="about-header">
-                <h2>About me</h2>
+            <div className="about-mask">
+                <div className="about-header">
+                    <h2>About me</h2>
+                </div>
+                <div className="about-content">
+                    <div className="about-content-box">
+                        <div className="about-image-border">
+                            <div className="about-image"></div>
+                        </div>
+                    </div>
+                    <div className="about-content-box right-box">
+                        <h3>Hi! My name is mateusz.</h3>
+                        <p className="about-txt">
+                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
+                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
+                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
+                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
+                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
+                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
+                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
+                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
+                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
+                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
+                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
+                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
+                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
+                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
+                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
+                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
+                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
+                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
+                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
+                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel                                Lorem blelele lelelle lelle lelel lell e llelelllelelelel
+                            Lorem blelele lelelle lelle lelel lell e llelelllelelelels
+                        </p>
+                    </div>
+                </div>               
             </div>
         </div>
     )
