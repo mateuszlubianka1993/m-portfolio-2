06282eae43411406c578e37837e0dcf2ebad45ee
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-09-27 10:39:23 +0200

    create Portfolio component and link it in Route

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Home from './Home';
 import NavBar from './NavBar';
 import About from './About';
 import Contact from './Contact';
+import Portfolio from './portfolio/Portfolio';
 
 const App = () => {
     return(
@@ -15,6 +16,7 @@ const App = () => {
                        <Route path="/" exact component={Home} /> 
                        <Route path="/about" component={About} />
                        <Route path="/contact" component={Contact} /> 
+                       <Route path="/portfolio" component={Portfolio} /> 
                     </Switch>
                 </div>
             </BrowserRouter>
