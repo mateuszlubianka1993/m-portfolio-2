9e7f5b5f5abd6a6887ddac704b469ef375e98777
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-09-27 10:52:38 +0200

    add data.js file with example data

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import data from '../../data/data';
 
 class Portfolio extends Component {
+    state = {
+        projects: data.projects,
+        project: [0]
+    };
+
     render() {
+        console.log(this.state.projects)
         return (
             <div>
                Portfolio 
---
+++ src/data/data.js
@@ -0,0 +1,18 @@
+const data = {
+    "projects": [
+        {
+            index: 0,
+            title: 'project1'
+        },
+        {
+            index: 1,
+            title: 'project2'
+        },
+        {
+            index: 2,
+            title: 'project3'
+        }
+    ]
+};
+
+export default data;
