785d753fd33521d8c1cd3714673fe62e07c43de3
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-09-29 11:44:49 +0200

    add buttons css

--- src/data/data.js
+++ src/data/data.js
@@ -3,17 +3,14 @@ const data = {
         {
             index: 0,
             title: 'project1',
-            id: 'proj-0'
         },
         {
             index: 1,
             title: 'project2',
-            id: 'proj-1'
         },
         {
             index: 2,
             title: 'project3',
-            id: 'proj-2'
         }
     ]
 };
