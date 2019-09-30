12cbc95a0b9642538d2af660dae4e87e11b71385
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-09-30 16:02:08 +0200

    add projects buttons

--- src/components/portfolio/Project.js
+++ src/components/portfolio/Project.js
@@ -6,7 +6,12 @@ const Project = ({project, active}) => {
     return (
         <div className={active === project.index ? 'project-container active' : 'project-container'} 
         style={{'backgroundImage': `url(${project.img})`}}>
-            {project.title}
+            <div className="project-mask"></div>
+            <h3 className="project-name">{project.title}</h3>
+            <div className="project-buttons">
+                <button className="project-btn"><i className="far fa-file-code"></i></button>
+                <button className="project-btn"><i className="far fa-eye"></i></button>
+            </div>
         </div>
     )
 }
