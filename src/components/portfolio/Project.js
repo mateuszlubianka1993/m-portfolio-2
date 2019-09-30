d60be03bd3258cf8a7488342a3a5a344735f008c
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-09-30 14:53:24 +0200

    display project img in Project component

--- src/components/portfolio/Project.js
+++ src/components/portfolio/Project.js
@@ -4,7 +4,8 @@ import '../../style/Project.css';
 const Project = ({project, active}) => {
     
     return (
-        <div className={active === project.index ? 'project-container active' : 'project-container'} >
+        <div className={active === project.index ? 'project-container active' : 'project-container'} 
+        style={{'backgroundImage': `url(${project.img})`}}>
             {project.title}
         </div>
     )
