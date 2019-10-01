14977b6c41fae5b3455619bd5b6a7cae3d05d7ef
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-10-01 16:49:28 +0200

    add "stack" property in data file

--- src/components/portfolio/Project.js
+++ src/components/portfolio/Project.js
@@ -6,9 +6,8 @@ const Project = ({project, active}) => {
     return (
         <div className={active === project.index ? 'project-container active' : 'project-container'} 
         style={{'backgroundImage': `url(${project.img})`}}>
-            {/* <div className="project-mask"></div> */}
             <h3 className="project-name">{project.title}</h3>
-            <p className="project-description">Jedno zdanie o projekcie. Albo jednak dwa.</p>
+            <p className="project-description">{project.stack}</p>
             <div className="project-buttons">
                 <a href={project.gitLink} target="_blank" rel="noopener noreferrer"><button className="project-btn"><i className="far fa-file-code"></i></button></a>
                 <button className="project-btn"><i className="fas fa-info"></i></button>
--- src/data/data.js
+++ src/data/data.js
@@ -5,35 +5,40 @@ const data = {
             title: 'News App',
             img: '/projects-img/news-app.jpg',
             gitLink: 'https://github.com/mateuszlubianka1993/news-app',
-            liveLink: 'https://mateuszlubianka1993.github.io/news-app/'
+            liveLink: 'https://mateuszlubianka1993.github.io/news-app/',
+            stack: 'React, Redux, Java Script, CSS, HTML, React-Router, React-Bootstrap, Axios, Redux-Thunk'
         },
         {
             index: 1,
             title: 'Tic Tac Toe Game',
             img: '/projects-img/tic-tac-toe-js.jpg',
             gitLink: 'https://github.com/mateuszlubianka1993/Tic-tac-toe-js-game',
-            liveLink: 'https://mateuszlubianka1993.github.io/Tic-tac-toe-js-game/index.html'
+            liveLink: 'https://mateuszlubianka1993.github.io/Tic-tac-toe-js-game/index.html',
+            stack: 'Java Script, CSS, HTML'
         },
         {
             index: 2,
             title: 'Shopping List V2',
             img: '/projects-img/shopping-list-v2.jpg',
             gitLink: 'https://github.com/mateuszlubianka1993/shopping-list-js-v2',
-            liveLink: 'https://mateuszlubianka1993.github.io/shopping-list-js-v2/'
+            liveLink: 'https://mateuszlubianka1993.github.io/shopping-list-js-v2/',
+            stack: 'Java Script, CSS, HTML, Firebase, Bootstrap'
         },
         {
             index: 3,
             title: 'The Movie App',
             img: '/projects-img/movie-app.jpg',
             gitLink: 'https://github.com/mateuszlubianka1993/the-movie-app',
-            liveLink: 'https://mateuszlubianka1993.github.io/the-movie-app/'
+            liveLink: 'https://mateuszlubianka1993.github.io/the-movie-app/',
+            stack: 'React, Redux, Java Script, CSS, HTML, React-Router, Axios, Semantic UI, Redux-Thunk'
         },
         {
             index: 4,
             title: 'Weather App',
             img: '/projects-img/weather-app-react.jpg',
             gitLink: 'https://github.com/mateuszlubianka1993/weather-app-react',
-            liveLink: 'https://mateuszlubianka1993.github.io/weather-app-react/'
+            liveLink: 'https://mateuszlubianka1993.github.io/weather-app-react/',
+            stack: 'React, Java Script, CSS, HTML'
         }
     ]
 };
