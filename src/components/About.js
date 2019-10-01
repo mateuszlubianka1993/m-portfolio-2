6a02301a72651153e3be473a4480a3ac0e2da836
Mateusz Łubianka <lubiankamateusz@gmail.com>
2019-10-01 15:01:03 +0200

    refactor About component

--- src/components/About.js
+++ src/components/About.js
@@ -5,42 +5,27 @@ const About = () => {
     return (
         <div className="about-container">
             <div className="about-mask">
-                <div className="about-header">
-                    <h2>About me</h2>
-                </div>
-                <div className="about-content">
-                    <div className="about-content-box">
-                        <div className="about-image-border">
-                            <div className="about-image"></div>
+                <div className="section-content">
+                    <div className="about-header">
+                        <h2>About me</h2>
+                    </div>
+                    <div className="about-content">
+                        <div className="about-content-box">
+                            <div className="about-image-border">
+                                <div className="about-image"></div>
+                            </div>
                         </div>
-                    </div>
-                    <div className="about-content-box right-box">
-                        <h3 className="about-txt-header">Hi! My name is mateusz.</h3>
-                        <p className="about-txt">
-                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
-                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
-                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
-                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
-                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
-                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
-                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
-                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
-                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
-                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
-                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
-                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
-                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
-                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
-                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
-                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
-                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
-                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
-                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel
-                            Lorem blelele lelelle lelle lelel lell e llelelllelelelel                                Lorem blelele lelelle lelle lelel lell e llelelllelelelel
-                            Lorem blelele lelelle lelle lelel lell e llelelllelelelels
-                        </p>
-                    </div>
-                </div>               
+                        <div className="about-content-box right-box">
+                            <h3 className="about-txt-header">Hi! My name is mateusz.</h3>
+                            <p className="about-txt">
+                                Lorem blelele lelelle lelle lelel lell e llelelllelelelel
+                                Lorem blelele lelelle lelle lelel lell e llelelllelelelel
+                                Lorem blelele lelelle lelle lelel lell e llelelllelelelel                                Lorem blelele lelelle lelle lelel lell e llelelllelelelel
+                                Lorem blelele lelelle lelle lelel lell e llelelllelelelels
+                            </p>
+                        </div>
+                    </div>          
+                </div>     
             </div>
         </div>
     )
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,50 +7,49 @@ const Contact = () => {
     return (
         <div className="contact-container">
             <div className="contact-mask">
-                <div className="contact-content">
-                <div className="contact-header">
-                    <h2>Contact me</h2>
-                    <p className="contact-email">lubiankamateusz@gmail.com</p>
-                </div>
-                <div className="social-container">
-                    <div className="social-box">
-                        <a href="https://github.com/mateuszlubianka1993" target="_blank" rel="noopener noreferrer">
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                            <span className="fab fa-github"></span>
-                        </a>
+                <div className="section-content">
+                    <div className="contact-header">
+                        <h2>Contact me</h2>
+                        <p className="contact-email">lubiankamateusz@gmail.com</p>
                     </div>
-                    <div className="social-box">
-                        <a href="https://www.linkedin.com/in/mateuszlubianka/" target="_blank" rel="noopener noreferrer">
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                            <span className="fab fa-linkedin-in"></span>
-                        </a>
-                    </div>
-                    <div className="social-box">
-                        <a href="https://twitter.com/Xiedzu" target="_blank" rel="noopener noreferrer">
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                            <span className="fab fa-twitter"></span>
-                        </a>
-                    </div>
-                    <div className="social-box">
-                        <a href="https://www.facebook.com/MateuszLubianka" target="_blank" rel="noopener noreferrer">
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                            <span className="fab fa-facebook-f fb-span"></span>
-                        </a>
-                    </div>
-                </div>
-                
+                    <div className="social-container">
+                        <div className="social-box">
+                            <a href="https://github.com/mateuszlubianka1993" target="_blank" rel="noopener noreferrer">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span className="fab fa-github"></span>
+                            </a>
+                        </div>
+                        <div className="social-box">
+                            <a href="https://www.linkedin.com/in/mateuszlubianka/" target="_blank" rel="noopener noreferrer">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span className="fab fa-linkedin-in"></span>
+                            </a>
+                        </div>
+                        <div className="social-box">
+                            <a href="https://twitter.com/Xiedzu" target="_blank" rel="noopener noreferrer">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span className="fab fa-twitter"></span>
+                            </a>
+                        </div>
+                        <div className="social-box">
+                            <a href="https://www.facebook.com/MateuszLubianka" target="_blank" rel="noopener noreferrer">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                                <span className="fab fa-facebook-f fb-span"></span>
+                            </a>
+                        </div>
+                    </div>                
                 </div>
                 <Particles params={particlesParams} className="particles-box"/>
             </div>
